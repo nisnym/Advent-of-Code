c3d7a0ef8d20053cf79920c7cb0ad53f30518369
Nishant Kumar <nisnym@gmail.com>
2020-12-21 17:55:41 +0530

    sol

---
+++ Day 6/solution.py
@@ -0,0 +1,22 @@
+  
+from collections import Counter
+
+with open('input.txt') as f:
+    for group in f.read().split('\n\n'):
+        group = group.strip()
+        letters = set(group)
+        letters.discard('\n')
+        
+        seen = Counter()
+        for person in group.split():
+            for c in person:
+                seen[c] += 1
+
+        one += len(letters)
+        two += len([
+            k for k, v in seen.items()
+            if v == group.count('\n') + 1
+        ])
+
+print('Part 1:', one)
+print('Part 2:', two)
